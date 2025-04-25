97d1ed2653fdde1f8a1d94c1d075967a5c6d6b07
Payal <roohmeiy@gmail.com>
2025-04-25 22:28:34 +0530

    modifed capacity

--- Terraform-ecs-cicd/main.tf
+++ Terraform-ecs-cicd/main.tf
@@ -113,8 +113,8 @@ resource "aws_ecs_task_definition" "strapi_task" {
   family                   = var.task_definition_name
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = "256"
-  memory                   = "512"
+  cpu                      = "1024"
+  memory                   = "2048"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([{
