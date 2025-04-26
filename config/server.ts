a624e54afd485f9de2fe3e2dc91aabf398cd88e2
Payal <roohmeiy@gmail.com>
2025-04-26 21:20:36 +0530

    removing blocked access

--- config/server.ts
+++ config/server.ts
@@ -1,17 +1,23 @@
+// export default ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+// });
+
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
-// });
-
-middlewares: {
-  settings: {
-    cors: {
-      enabled: true,
-      origin: ['http://payal-strapi-alb-1496762997.us-east-1.elb.amazonaws.com', 'http://localhost:1337', 'http://localhost:3000']
+  middlewares: {
+    settings: {
+      cors: {
+        enabled: true,
+        origin: ['http://payal-strapi-alb-1496762997.us-east-1.elb.amazonaws.com', 'http://localhost:1337', 'http://localhost:3000']
+      }
     }
   }
-}
 });
