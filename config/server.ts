31a66d73960975b25796611872fd17c9251f4abc
Payal <roohmeiy@gmail.com>
2025-04-26 21:10:07 +0530

    removing blocked access

--- config/server.ts
+++ config/server.ts
@@ -4,4 +4,14 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+// });
+
+middlewares: {
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ['http://payal-strapi-alb-1496762997.us-east-1.elb.amazonaws.com', 'http://localhost:1337', 'http://localhost:3000']
+    }
+  }
+}
 });
