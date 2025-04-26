d955c146e19177be6ece21b2b5f19d29292ee8c3
Payal <roohmeiy@gmail.com>
2025-04-26 21:42:49 +0530

    removing blocked access

--- config/server.ts
+++ config/server.ts
@@ -7,17 +7,19 @@
 // });
 
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: '0.0.0.0',
+  port: 1337,
   app: {
     keys: env.array('APP_KEYS'),
   },
-  middlewares: {
-    settings: {
-      cors: {
-        enabled: true,
-        origin: ['http://payal-strapi-alb-1496762997.us-east-1.elb.amazonaws.com', 'http://localhost:1337', 'http://localhost:3000']
-      }
-    }
-  }
+  url: 'http://payal-strapi-alb-1496762997.us-east-1.elb.amazonaws.com',
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+    url: '/admin',
+  },
+  webhooks: {
+    populateRelations: false,
+  },
 });
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+export default {
+    server: {
+      allowedHosts: [
+        'payal-strapi-alb-1496762997.us-east-1.elb.amazonaws.com'
+      ]
+    }
+  }
