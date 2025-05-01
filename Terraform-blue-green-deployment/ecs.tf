5e79821dc335d2dfd18ca1207866ce367af05d3d
Payal <roohmeiy@gmail.com>
2025-05-01 19:02:39 +0530

    modularize code

--- Terraform-blue-green-deployment/ecs.tf
+++ Terraform-blue-green-deployment/ecs.tf
@@ -44,3 +44,45 @@ resource "aws_ecs_task_definition" "strapi_task" {
     ]
   }])
 }
+
+# ECS Service
+resource "aws_ecs_service" "strapi_service" {
+  name            = var.ecs_service_name
+  cluster         = aws_ecs_cluster.strapi_cluster.id
+  task_definition = aws_ecs_task_definition.strapi_task.arn
+  desired_count   = 1
+  
+  # Use capacity provider strategy for Fargate Spot
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+    weight            = 100
+  }
+
+  network_configuration {
+    subnets          = aws_subnet.strapi_public_subnet[*].id
+    security_groups  = [aws_security_group.strapi_sg.id]
+    assign_public_ip = true
+  }
+
+  # Connect to the blue target group initially
+  load_balancer {
+    target_group_arn = aws_lb_target_group.blue_tg.arn
+    container_name   = "strapi-app"
+    container_port   = 1337
+  }
+  
+  # Set deployment controller to CODE_DEPLOY for blue/green deployments
+  deployment_controller {
+    type = "CODE_DEPLOY"
+  }
+
+  # This will ensure proper ordering of resource creation/deletion
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [task_definition, load_balancer]
+  }
+
+  depends_on = [
+    aws_lb_listener.strapi_listener_prod
+  ]
+}
